fe3d4f471b99e7f4cde48ba138a0e39c29d00560
gurvir dhillon <gdhillon@sfsu.edu>
2025-12-23 01:12:35 -0800

    removed unnecessary playerrespawnevent import

--- src/main/java/shared/health/listener/DeadListener.java
+++ src/main/java/shared/health/listener/DeadListener.java
@@ -4,7 +4,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 
 import shared.health.manager.DeadManager;
 
